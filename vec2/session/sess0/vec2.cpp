474a7bdf72d671e0fbac34b08e700bae8aac4201
simon231442 <simon2314@hotmail.com>
2026-06-05 22:04:16 +0200

    lenovo : vec2 : sess0 quasi fini

--- vec2/session/sess0/vec2.cpp
+++ vec2/session/sess0/vec2.cpp
@@ -23,3 +23,24 @@ vec2	operator+(vec2 const & rhs) const
 {
 	return vec2(container_[0] + rhs.container_[0], container_[1] + rhs.container_[1]);
 }
+
+vec2	operator-(vec2 const & rhs) const
+{
+	return vec2(container_[0] - rhs.container_[0], container_[1] - rhs.container_[1]);
+}
+
+vec2 	operator*(int scalair) const
+{
+	return vec2(container_[0] * scalair, container_[1] * scalair);
+}
+
+int		vec2::getContainer_elements(int index) const
+{	
+	if (index < 0 || index > 1)
+		throw std::out_of_range("index must be 1 or 0");
+	return container_[static_cast<std::size_t>(index)];
+}
+
+std::ostream&	operator<<(std::ostream & out, vec2 const & v)
+{
+
