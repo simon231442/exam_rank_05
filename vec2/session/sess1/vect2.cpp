bfd8b661dea41e0d7b878d9ccf51059f8b5dd237
simon231442 <simon2314@hotmail.com>
2026-06-11 10:19:43 +0200

    lenovo : sess0 : rest la def de operator <<

--- vec2/session/sess1/vect2.cpp
+++ vec2/session/sess1/vect2.cpp
@@ -14,11 +14,27 @@ vect2::~vect2() {}
 
 vect2&		vect2::operator=(vect2 const & rhs) {
 	if (this != &rhs)
-		container_ = rhs->container_;
+		container_ = rhs.container_;
 	return *this;
 }
 
-vect2		vect2::operator+(vect2 const & rhs) {
+vect2		vect2::operator+(vect2 const & rhs) const {
+	return vect2(container_[0] + rhs.container[0], container_[1] + rhs.container[1]);
+}
+
+vect2		vect2::operator-(vect2 const & rhs) const {
+	return vect2(container_[0] - rhs.container[0], container_[1] - rhs.container[1]);
+}
+
+vect2		vect2::operator*(int scalar) const {
+	return vect2(container_[0] * scalar, container_[1] * scalar);
+}
+
+int			vect2::getContainer_elements(int index) const {
+	if (index < 0 || index > 1)
+		throw std::out_of_range("index must be between 0 1");
+	return static<int>(container_[index]);
+}
 
 
 
