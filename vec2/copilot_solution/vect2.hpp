ffea58df0589ee9595b6e14a942809a881209437
simon231442 <simon2314@hotmail.com>
2026-06-18 11:05:41 +0200

    lenovo au lac : lignage 2/2

--- vec2/copilot_solution/vect2.hpp
+++ vec2/copilot_solution/vect2.hpp
@@ -9,34 +9,34 @@ class vect2
     private:
         std::vector<int> container_;
     public:
-        vect2();
-        vect2(int num1, int num2);
-        vect2(const vect2& source);
-        vect2& operator=(const vect2& source);
+        vect2();//:23??????
+        vect2(int num1, int num2);//:24
+        vect2(const vect2& source);//:25
+        vect2& operator=(const vect2& source);//:26
 
-        int operator[](int index) const;
-        int& operator[](int index);
+        int operator[](int index) const;//:49,50
+        int& operator[](int index);//:47,48
 
         vect2 operator-() const;//:46
-        vect2 operator*(int num) const;
+        vect2 operator*(int num) const;//:39
 
-        vect2& operator*=(int num);
+        vect2& operator*=(int num);//:42
 
         vect2& operator+=(const vect2& obj);//:37
         vect2& operator-=(const vect2& obj);//:38
-        vect2& operator*=(const vect2& obj);
+        vect2& operator*=(const vect2& obj);//!!!!!!!!!!!!!!!!!!!!!!!!
 
-        vect2 operator+(const vect2& obj) const;
+        vect2 operator+(const vect2& obj) const;//:43
         vect2 operator-(const vect2& obj) const;//:43
-        vect2 operator*(const vect2& obj) const;
+        vect2 operator*(const vect2& obj) const;//:!!!!!!
 
-        vect2& operator++();
-        vect2 operator++(int);
+        vect2& operator++();//:34
+        vect2 operator++(int);//:33
         vect2& operator--();//:36
         vect2 operator--(int);//:35
 
-        bool operator==(const vect2& obj) const;
-        bool operator!=(const vect2& obj) const;
+        bool operator==(const vect2& obj) const;//:51,52
+        bool operator!=(const vect2& obj) const;//:53,54
 
         ~vect2();
 };
