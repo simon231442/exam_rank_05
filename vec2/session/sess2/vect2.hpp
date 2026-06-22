f589b9a8c5922ae744352c665f30eb062fc41660
simon231442 <simon2314@hotmail.com>
2026-06-22 21:23:18 +0200

    lenovo : vect2 : sess2 : tout methode mise mais a verifier

--- vec2/session/sess2/vect2.hpp
+++ vec2/session/sess2/vect2.hpp
@@ -21,11 +21,23 @@ class	vect2 {
 		vect2				operator*(int scalar) const;	// v2 = v1 * 3;
 
 		vect2				operator*=(int scalar);			// v *= 3;
-		vect2				operator*=(vect2 const & rhs);
-		vect2				operator+=(vect2 const & rhs);// pourquoi pas de += un scalaire ?
-		vect2				operator-=(vect2 const & rhs);
+		//vect2				operator*=(vect2 const & rhs);	// v1 *= v2;
+		vect2				operator+=(vect2 const & rhs);	// v1 += v2;
+		vect2				operator-=(vect2 const & rhs);	// v1 -= v2;
+
+		vect2&				operator++();					//++v; pourquoi ici &
+		vect2				operator++(int);				//v++; et pas la ???
+		vect2&				operator--();					//--v;
+		vect2				operator--();
+
+		bool				operator==(vect2 const& rhs) const; // bool = (v1 == v2)
+		bool				operator!=(vect2 const& rhs) const; // bool = (v1 != v2)
+
 
 		~vect2();
 	};
 
 int			operator*(int scalar, vect2 const & rhs);
+std::ostream&				operator<<(std::ostream& out, vect2 const & rhs);
+
+#endif
