92726fec122aea863b3548af21d837d39223112d
Simon Renaud <srenaud@c3r5s3.42lausanne.ch>
2026-09-01 07:33:48 +0200

    ssd : bigint : sess0 : toAdd + less + opertaeur +

--- bigint/session/sess0/bigint.cpp
+++ bigint/session/sess0/bigint.cpp
@@ -28,4 +28,41 @@ std::string bigint::add(std::string const & a, std::string const & b)
 		int			sum = carry;//sum et carry?
 		if (i < a.size())
 			sum += a[a.size() - 1 - i] - '0'; //est-ce ok avec les type
+		if (i < b.size())
+			sum += b[b.size() - 1 - i] - '0';
+		result.pushback(static_cast<char>(sum % 10 + '0');
+		carry = sum / 10;
+	}
+	std::reverse(result.begin(), result.end());
+	return result;
+}
 
+std::bool	bigint::less(std::string const & a, std::string const & b)
+{
+	if (a.size() != b.size())
+		return (a.size() < b.size())
+	return (a < b);
+}
+
+unsigned long long	bigint::toULL(std::string const & s)
+{
+	unsigned long long max = std::numeric_limits<unsigned long long>::max();//a retenir par coeur
+	unsigned long long n = 0;
+
+	for (std::string::size_type i = 0; i < s.size(); ++i)
+	{
+		unsigned long long d = static_cast<unsigned long long>(s[i] - 0);
+		if (n > (max - d) / 10)
+			return max;
+		n = n * 10 + d;
+	}
+	return (n);
+}
+
+
+bigint		bigint::operator+(bigint const & rhs) const
+{
+	bigint	result(*this);
+	return	result += rhs;
+}
+bigint&		bigint::operator+=(bigint const & rhs)
