0f8a828b86b105f9b9b975a51b56bdbbac4f4a1d
simon231442 <simon2314@hotmail.com>
2026-09-06 21:32:50 +0200

    lenovoMaison : vect2 : sess4 : hpp pret (from scratch)

---
+++ vec2/session/sess4/vect2.hpp
@@ -0,0 +1,38 @@
+#ifndef VECT2_HPP
+# define VECT2_HPP
+
+#include <iostream>
+#include <vector>
+
+class vect2 {
+	public :
+		std::vector		vec;
+
+		vect2();
+		vect2(int x, int y);
+		vect(vect2 const & src);
+		~vect2();
+		vect2			operator=(vect2 const & rhs);
+
+		vect2			operator+(vect2 const & rhs);
+		vect2			operator-(vect2 const & rhs);
+		vect2			operator*(int scalar);
+		
+		vect2			operator++(int);
+		vect2			operator++();
+		vect2			operator--(int);
+		vect2			operator--();
+		
+		vect2			operator+=(vect2 const & rhs);
+		vect2			operator-=(vect2 const & rhs);
+		vect2			operator*=(int scalar)
+		
+		vect2 &			operator[](int index)
+		vect2			operator[](int index)
+	friend :
+		std::ostream	operator<<(std::ostream out; vect2 vec);
+		vect2			operator*(int scalar);
+		vect2			operator+(int scalar);
+	
+
+
