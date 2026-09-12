96128e6c667a5f543ee82f1ded3334d2434cfded
Simon Renaud <srenaud@c2r5s6.42lausanne.ch>
2026-09-12 10:07:05 +0200

    asgard : vect2 : const ajouter et ou il fallait + operateur++ fait + doc cpp telecharge

--- vec2/session/sess4/vect2.cpp
+++ vec2/session/sess4/vect2.cpp
@@ -28,69 +28,72 @@ vect2			vect2::operator=(vect2 const & rhs)
 	return *this;
 }
 
-vect2			vect2::operator+(vect2 const & rhs)
+vect2			vect2::operator+(vect2 const & rhs) const
 {
 	return (vect2(this->vec_[0] + rhs.vec_[0], this->vec_[1] + rhs.vec_[1]);
 }
-vect2			vect2::operator-(vect2 const & rhs)
+
+vect2			vect2::operator-(vect2 const & rhs) const
 {
 	return (vect2(this->vec_[0] - rhs.vec_[0], this->vec_[1] - rhs.vec_[1]);
 }
-vect2			vect2::operator*(int scalar)
+
+vect2			vect2::operator*(int scalar) const
 {
 	return (vect2(this->vec_[0] * scalar, this->vec_[1] * scalar);
 }
 
 vect2			vect2::operator++(int)
 {
-	
+	vect2	tmp(*this);
+	++*this;
 	return tmp;
 }
+
 vect2			vect2::operator++()
 {
-
+	++vec_[0];
+	++vec_[1];
+	return *this;
 }
+
 vect2			vect2::operator--(int)
 {
-
 }
+
 vect2			vect2::operator--()
 {
-
 }
 
 vect2			vect2::operator+=(vect2 const & rhs)
 {
-
 }
+
 vect2			vect2::operator-=(vect2 const & rhs)
 {
-
 }
-vect2			vect2::operator*=(int scalar
+
+vect2			vect2::operator*=(int scalar)
 {
-
 }
 
-vect2 &			vect2::operator[](int index
+vect2 &			vect2::operator[](int index) const
 {
-
 }
-vect2			vect2::operator[](int index
+
+vect2			vect2::operator[](int index) const
 {
-
 }
 
-std::ostream	vect2::operator<<(std::ostream out; vect2 vec)
+std::ostream	vect2::operator<<(std::ostream out; vect2 vec) const
 {
-
 }
+
 vect2			vect2::operator*(int scalar)
 {
-
 }
+
 vect2			vect2::operator+(int scalar)
 {
-
 }
 
--- vec2/session/sess4/vect2.hpp
+++ vec2/session/sess4/vect2.hpp
@@ -14,21 +14,21 @@ class vect2 {
 		~vect2();
 		vect2			operator=(vect2 const & rhs);
 
-		vect2			operator+(vect2 const & rhs);
-		vect2			operator-(vect2 const & rhs);
-		vect2			operator*(int scalar);
+		vect2			operator+(vect2 const & rhs) const;
+		vect2			operator-(vect2 const & rhs) const;
+		vect2			operator*(int scalar) const;
 		
 		vect2			operator++(int);
 		vect2			operator++();
 		vect2			operator--(int);
 		vect2			operator--();
-		
+
 		vect2			operator+=(vect2 const & rhs);
 		vect2			operator-=(vect2 const & rhs);
-		vect2			operator*=(int scalar)
+		vect2			operator*=(int scalar const)
 		
-		vect2 &			operator[](int index)
-		vect2			operator[](int index)
+		vect2 &			operator[](int index) const;
+		vect2			operator[](int index) const;
 	friend :
 		std::ostream	operator<<(std::ostream out; vect2 vec);
 		vect2			operator*(int scalar);
