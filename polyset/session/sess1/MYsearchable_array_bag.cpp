945af569e9963a39c89b2d42548e01fdb4c557a6
Simon Renaud <srenaud@c2r5s6.42lausanne.ch>
2026-09-14 09:00:32 +0200

    asgard : polyset : sess1 : searchable_array_bag .cpp et .hpp fait

---
+++ polyset/session/sess1/MYsearchable_array_bag.cpp
@@ -0,0 +1,25 @@
+#include "MYsearchable_array_bag.hpp"
+
+searchable_array_bag::searchable_array_bag() {}
+
+searchable_array_bag::searchable_array_bag(searchable_array_bag const & src) : array_bag(src) { };
+
+searchable_array_bag &		searchable_array_bag::operator=(searchable_array_bag const & rhs)
+{
+	if (this != &src)
+		searchable_bag::operator=(rhs);
+	return *this;
+}
+
+searchable_array_bag::~searchable_array_bag() {}
+
+bool						searchable_array_bag::has(int value) const
+{
+	for (int i = 0; i < size; i++)
+	{
+		if (data[i] == value)
+			return true;
+	}
+	return false;
+}
+
---
+++ polyset/session/sess1/MYsearchable_array_bag.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "array_bag.hpp"
+#include "searchable_bag.hpp"
+
+class searchable_array_bag.hpp : public array_bag, public searchable_bag {
+	public :
+		searchable_array_bag();
+		searchable_array_bag(searchable_array_bag const & src);
+		searchable_array_bag &	operator=(searchable_array_bag const & rhs);
+		~searchable_array_bag();
+		bool					has(int) const;
+}
