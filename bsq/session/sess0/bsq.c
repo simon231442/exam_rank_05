ca8bd2334a936552ad4a59f920a3d50916eb9783
Simon Renaud <srenaud@c2r7s6.42lausanne.ch>
2026-09-18 09:56:56 +0200

    asgarde : bsq : sess0 : fullman : fullM en cours

--- bsq/session/sess0/bsq.c
+++ bsq/session/sess0/bsq.c
@@ -7,9 +7,11 @@ int		n;
 char	em;
 char	ob;
 char	fu;
+char	**map;
 
 void	err(void) {fprintf(stderr, "map error\n"); }
 int		getK(void);
+int		fillM(void);
 
 int main(int ac, char *av[])
 {
@@ -25,6 +27,9 @@ int main(int ac, char *av[])
 	}
 	if (getK())
 		return (err(), 1);
+	if (fillM())
+		return (err(), 1);
+	return 0;
 	//get et parse la 1 ligne
 	//creer le tableau de de char
 	//creer le tableau d'int
@@ -35,7 +40,7 @@ int main(int ac, char *av[])
 
 int		getK(void)
 {
-	size_t	size;
+	size_t	size = 0;
 	int		i = 0;
 
 	getline (&line, &size, file);
@@ -61,9 +66,40 @@ int		getK(void)
 	if (line[i] < 32 || line[i] > 126 || line[i] == em || line[i] == ob)
 		return 1;
 	fu = line[i++];
-	fprintf(stdout, "%d %c %c %c\n", n, em, ob, fu);
+//	fprintf(stdout, "%ld", size);
+//	fprintf(stdout, "%d %c %c %c\n", n, em, ob, fu);
+	free(line);
 	return 0;
 }
+
+int		check(* line);
+int		fillM(void)
+{
+	size_t	size = 0;
+	int		l;
+
+	map = calloc(n, sizeof(char**));
+		if (!map)
+			return (freeA, 1);
+	l = (int)getline(&map[0], &size, file);
+	if (!map[i])
+		return (freeA, 1);
+	for (int i = 1; i < n; ++i)
+	{
+		if (l != getline(&map[i], &size, file) || !map[i] || check(map[i]))
+			return (freeA, 1);
+	}
+}
+
+int		check(char* line)
+{
+	while (*line && *line != '\n')
+	{
+		if (line[i] != em || line[i] != ob || line[i] != fu)
+			return (freeA, 1);
+	}
+
+	
 
 
 
