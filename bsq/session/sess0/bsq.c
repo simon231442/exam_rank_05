4f9e452a7df65daf7125d39e805cf99dfdf34ee4
Simon Renaud <srenaud@c2r7s6.42lausanne.ch>
2026-09-18 08:50:06 +0200

    asgard : bsq : sess0 : full man : 1 ligne parse

--- bsq/session/sess0/bsq.c
+++ bsq/session/sess0/bsq.c
@@ -2,9 +2,14 @@
 #include <stdio.h>
 
 FILE	*file;
-char	*ligne;
+char	*line = NULL;
+int		n;
+char	em;
+char	ob;
+char	fu;
 
 void	err(void) {fprintf(stderr, "map error\n"); }
+int		getK(void);
 
 int main(int ac, char *av[])
 {
@@ -14,10 +19,12 @@ int main(int ac, char *av[])
 		file = stdin;
 	if (ac == 2)
 	{
-		file = fopen(av[1], r);
+		file = fopen(av[1], "r");
 		if (!file)
 			return (err(), 1);
 	}
+	if (getK())
+		return (err(), 1);
 	//get et parse la 1 ligne
 	//creer le tableau de de char
 	//creer le tableau d'int
@@ -26,3 +33,39 @@ int main(int ac, char *av[])
 	//afficher
 }
 
+int		getK(void)
+{
+	size_t	size;
+	int		i = 0;
+
+	getline (&line, &size, file);
+	if (!line)
+		return 1;
+	if (line[i] < '1' || line[i] > '9')
+		return 1;
+	n = line[i++] - '0';
+	if (!(line[i] < '1' || line[i] > '9'))
+		n = n * 10 + line[i++] - '0';
+	if (line[i++] != ' ')
+		return 1;
+	if (line[i] < 32 || line[i] > 126)
+		return 1;
+	em = line[i++];
+	if (line[i++] != ' ')
+		return 1;
+	if (line[i] < 32 || line[i] > 126 || line[i] == em)
+		return 1;
+	ob = line[i++];
+	if (line[i++] != ' ')
+		return 1;
+	if (line[i] < 32 || line[i] > 126 || line[i] == em || line[i] == ob)
+		return 1;
+	fu = line[i++];
+	fprintf(stdout, "%d %c %c %c\n", n, em, ob, fu);
+	return 0;
+}
+
+
+
+	
+	//ssize_t getline(char **lineptr, size_t *n, FILE *stream);
