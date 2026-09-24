ee92dceeeb6b15ed9001f4ca18b912959635787f
Simon Renaud <srenaud@c2r7s7.42lausanne.ch>
2026-09-24 11:36:46 +0200

    asgard : jour dexam : bsq : sess1 : min remplace par max

--- bsq/session/sess1/bsq.c
+++ bsq/session/sess1/bsq.c
@@ -9,6 +9,11 @@ char	full;
 int		w;
 char	*line = NULL;
 size_t	bit = 0;
+int		map[100][100];
+int		one = 0;
+int		lb = 0;
+int		k;
+int		l;
 
 //int fprintf(FILE *stream, const char *format, ...);
 void	printE(void) {fprintf(stdout, "map error\n");}
@@ -39,16 +44,20 @@ int		main(int ac, char *av[])
 }
 
 int		setK(void);
+int		parse(void);
+void	findB(void);
 void	solve(void)
 {
 	if (setK())
 		return(printE());
+	if (parse())
+		return(printE());
+	findB();
 }
 
 int		setK(void)
 {
 	int	ret = fscanf(file, "%d %c %c %c", &h, &emp, &obs, &full);
-	//ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 	//int fscanf(FILE *stream, const char *format, ...);
 	if (ret != 4)
 		return 1;
@@ -67,6 +76,88 @@ int		setK(void)
 //	printf("%s",line);
 	free(line);
 	line = NULL;
+	bit = 0;
 	return 0;
 }
 
+int		fillM(int i);
+int		parse(void)
+{
+	for (int i = 0; i < h; ++i)
+	{
+		if (i == 0)
+			w = (int)getline(&line, &bit, file);
+		else
+		{
+			if (w != (int)getline(&line, &bit, file))
+			{
+				free(line);
+				return 1;
+			}
+		}
+		if (fillM(i))
+			return 1;
+		free(line);
+		line = NULL;
+		bit = 0;
+	}
+	return 0;
+}
+
+int		fillM(int i)
+{
+	for (int j = 0; j < w; ++j)
+	{
+		if (line[i] == emp)
+		{
+			map[i][j] = 1;
+			one = 1;
+		}
+		else if (line[i] == obs)
+			map[i][j] = 0;
+		else
+			return 1;
+	}
+	return 0;
+}
+
+int		myMin(int i, int j);
+void	findB(void)
+{
+	for (int i = 0; i < h; ++i)
+	{
+		for (int j = 0; j < w; ++j)
+		{
+			if ((j == 0 || i == 0) && map[i][j])
+				break;
+			else if (map[i][j])
+			{
+				map[i][j] = myMin(i, j) + 1;
+				if (map[i][j] > lb)
+				{
+					lb = map[i][j];
+					k = i;
+					l = j;
+				}
+			}
+		}
+	}
+}
+
+int		myMin(int i, int j)
+{
+	int	up;
+	int dia;
+	int le;
+	int min;
+
+	up = map[i - 1][j];
+	dia = map[i - 1][j - 1];
+	le = map[i][j - 1];
+	min = (up <= dia) ? up : dia;
+	min = (min <= le) ? min : le;
+	return min;
+}
+
+//gerer le \n
+//checker le one
